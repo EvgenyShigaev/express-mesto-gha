a7654e54d10652339ac2d899d1d4135b9e9a947f
Evgeny Shigaev <shigaev-evgeniy@yandex.ru>
2023-08-08 20:21:02 +0400

    14

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+
 const mongoose = require('mongoose');
 // const helmet = require('helmet');
 const { errors } = require('celebrate');
-const router = require('./routes');
+const routes = require('./routes');
 
 // const auth = require('./middlewares/auth');
 // const limiter = require('./middlewares/rateLimit');
-const internalServerError = require('./middlewares/internalServerError');
+const error500 = require('./middlewares/error500');
 
+const { PORT = 3000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
@@ -15,16 +17,19 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 // const errorNotFound = require('./routes/errorNotFound');
 const authRoutes = require('./routes/auth');
 
-const { PORT = 3000 } = process.env;
 const app = express();
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+app.use(error500);
 
 app.use(authRoutes);
 // app.use(auth);
-app.use(router);
+
 // app.use(errorNotFound);
-app.use(errors());
-app.use(express.json());
-app.use(internalServerError);
+
+
+
 
 // app.use(helmet());
 // app.use(limiter);
