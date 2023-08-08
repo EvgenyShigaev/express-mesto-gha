7dc99289e3ff6fc2688140f3551cbb38eee392f1
Evgeny Shigaev <shigaev-evgeniy@yandex.ru>
2023-08-08 19:15:26 +0400

    14

--- app.js
+++ app.js
@@ -2,30 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 // const helmet = require('helmet');
 const { errors } = require('celebrate');
-const auth = require('./middlewares/auth');
-const internalServerError = require('./middlewares/internalServerError');
-
 const router = require('./routes');
 
+// const auth = require('./middlewares/auth');
 // const limiter = require('./middlewares/rateLimit');
+const internalServerError = require('./middlewares/internalServerError');
 
 const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
 const app = express();
 
-router.use(auth);
-app.use(router);
-
 app.use(express.json());
+// router.use(auth);
+app.use(router);
 
 app.use(errors());
 app.use(internalServerError);
 
 // app.use(helmet());
 // app.use(limiter);
-
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
-  useNewUrlParser: true,
-});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
